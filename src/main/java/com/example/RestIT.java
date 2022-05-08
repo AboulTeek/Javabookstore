6da3a07af63b9806e340a7bdfeb6dd8917f35755
Tarek Nasr <tarek.a.nasr@gmail.com>
2022-05-08 19:43:38 +0200

    a fix to RestIT.java

--- src/main/java/com/example/RestIT.java
+++ src/main/java/com/example/RestIT.java
@@ -1,13 +1,3 @@
-package com.baeldung.maven.it;
-
-import org.glassfish.jersey.server.ResourceConfig;
-
-public class EndpointConfig extends ResourceConfig {
-    public EndpointConfig() {
-        register(RestEndpoint.class);
-    }
-}
-
 @Path("/")
 public class RestEndpoint {
     @GET
